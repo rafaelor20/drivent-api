539e42d27ddd71745b6ee6453a8e6dbc0844f9a1
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2023-04-16 18:51:30 -0300

    rota tickets

--- src/routers/tickets-router.ts
+++ src/routers/tickets-router.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import { authenticateToken } from '@/middlewares';
-import { getTicketTypes } from '@/controllers/tickets-controller';
+import { getTicketTypes, getTickets } from '@/controllers/tickets-controller';
 
 const ticketsRouter = Router();
 
-ticketsRouter.get('/types', getTicketTypes).get('/').all('/*', authenticateToken);
+ticketsRouter.all('/*', authenticateToken).get('/types', getTicketTypes).get('/').get('/', getTickets);
 
 export { ticketsRouter };
